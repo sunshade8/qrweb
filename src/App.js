aac4cfec5876c4cee6efed4705adf90552d762dc
sunshade8 <stevenpark119@gmail.com>
2024-11-12 23:54:37 +0900

    responsive web

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import { useMediaQuery } from 'react-responsive'
 import React from 'react';
 import './App.css';
 import Main from './Main';
- 
+import { useMediaQuery } from 'react-responsive';
+
 const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 992 })
-  return isDesktop ? children : null
-}
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  return isDesktop ? children : null;
+};
 const Tablet = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-  return isTablet ? children : null
-}
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  return isTablet ? children : null;
+};
 const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 })
-  return isMobile ? children : null
-}
-const Default = ({ children }) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 })
-  return isNotMobile ? children : null
-}
- 
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+
 const App = () => (
   <div>
-    <Desktop>Desktop or laptop</Desktop>
-    <Tablet>Tablet</Tablet>
-    <Mobile><Main/></Mobile>
-    <Default>Not mobile (desktop or laptop or tablet)</Default>
+    <Main />
   </div>
-)
- 
+);
+
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -11,39 +11,41 @@ import InfoSection from './InfoSection';
 
 function Main() {
   return (
-    <div className='Main'
-        style={{
+    <div
+      className="Main"
+      style={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        }}
+      }}
     >
-      <div style={{paddingTop:'20px'}}></div>
-        <div className='Main_header'
-            style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-            }}
-        >
-            <p style={{fontWeight:'bold', fontSize:'20px'}}>QRMENU</p>
-            <div style={{width:'130px'}}></div>
-            <Button
-                variant="contained"
-                style={{width: '120px',fontSize:'14px',fontWeight:'bold'}}
-                sx={{backgroundColor:'#4942E4',borderRadius:'30px'}}>
-                    지금 신청하기
-                </Button>
-        </div>  
-        <Section1/>
-        <img src={phoneImage} alt="Example" className='resizable-image' />
-        <Section2/>
-        <Section3/>
-        <Section4/>
-        <Section5/>
-        <InfoSection/>
-     
+      <div className="Main_header"
+          style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '90%',
+              padding: '20px',
+              paddingLeft: '20px'
+          }}
+      >
+          <p style={{ fontWeight: 'bold', fontSize: '20px' }}>QRMENU</p>
+          <Button
+              variant="contained"
+              style={{ width: '120px', fontSize: '14px', fontWeight: 'bold' }}
+              sx={{ backgroundColor: '#4942E4', borderRadius: '30px' }}
+          >
+              지금 신청하기
+          </Button>
+      </div>  
+      <Section1 />
+      <img src={phoneImage} alt="Example" className="resizable-image" />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <InfoSection />
     </div>
   );
 }
