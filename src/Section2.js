2cb9bb943bfa5e05975b4e2b7290d98c229fa3bb
sunshade8 <stevenpark119@gmail.com>
2025-02-05 03:01:50 +0900

    order edit

--- src/Section2.js
+++ src/Section2.js
@@ -35,6 +35,24 @@ function Section2(){
             <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',color:"#CC3F3F",paddingTop:"10px",}}>커스텀 디자인의 웹사이트 제작</p>
             <p className='responsive-text-b' style={{textAlign:'center',fontSize:"14px",fontWeight:'bold',paddingTop:"20px",}}>✨✨ 웹사이트는 무제한으로 수정 가능해요</p>
             <p className='responsive-text-b' style={{textAlign:'center',fontSize:"14px",fontWeight:'bold',color:"#5C5B5B",paddingTop:"10px",}}>⚠️ 금액은 추가 협의, 요구사항 별 상이</p>
+            <div style={{ display: 'flex', justifyContent: 'center' ,paddingTop: '70px'}}>
+            <img
+                ref={imageRef}
+                src={ipadCurrent} // Ensure this is the correct path or URL to your image
+                alt="QR Order"
+                    style={{
+                        width: '90%',
+                        maxWidth: '500px', // limits the size on larger screens
+                        borderRadius: '15px', // rounded corners for the image
+                        justifyContent: 'center', // center the image (mostly for flex containers)
+                        objectFit: 'cover', // ensures the aspect ratio is maintained while filling the area
+                     }}
+            />
+
+            </div>
+            <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"70px",}}>기계를 잘 못다루는 분들도</p>
+            <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"10px",}}>바로 적응할 수 있는</p>
+            <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',color:"#CC3F3F",paddingTop:"10px",}}>매장 관리 화면</p>
             <div style={{paddingTop:'70px'}}></div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
             <img
@@ -54,24 +72,7 @@ function Section2(){
             <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"70px",}}>지금 신청시</p>
             <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"10px",}}>3개월간 주문 & 분석</p>
             <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',color:"#CC3F3F",paddingTop:"10px",}}>✨무료!✨</p>
-            <div style={{ display: 'flex', justifyContent: 'center' ,paddingTop: '70px'}}>
-            <img
-                ref={imageRef}
-                src={ipadCurrent} // Ensure this is the correct path or URL to your image
-                alt="QR Order"
-                    style={{
-                        width: '90%',
-                        maxWidth: '500px', // limits the size on larger screens
-                        borderRadius: '15px', // rounded corners for the image
-                        justifyContent: 'center', // center the image (mostly for flex containers)
-                        objectFit: 'cover', // ensures the aspect ratio is maintained while filling the area
-                     }}
-            />
-
-            </div>
-            <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"70px",}}>기계를 잘 못다루는 분들도</p>
-            <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"10px",}}>바로 적응할 수 있는</p>
-            <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',color:"#CC3F3F",paddingTop:"10px",}}>매장 관리 화면</p>
+            
             <p className='responsive-text-s' style={{textAlign:'center',fontWeight:'bold',paddingTop:"50px",color:"#4E5968",}}>❗️곧 추가될 기능❗️</p>
             <p className='responsive-text-b' style={{textAlign:'center',fontWeight:'bold',paddingTop:"50px",}}>AI 데이터 분석 서비스</p>
             
